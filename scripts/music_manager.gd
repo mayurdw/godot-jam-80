746f14682154d80e67e0f8560a184d6fc20553fe
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-04-18 13:47:32 +1200

    Tracks and default scene reverted

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -3,18 +3,25 @@ extends Node
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var rhythm_notifier: RhythmNotifier = $RhythmNotifier
 
-@export var stream = preload("res://assets/sounds/82/Billy's Sacrifice.mp3")
+@export var tracks: Array[Track]
 
+var index = 0
 signal on_beat
 
 func _ready() -> void:
-	rhythm_notifier.bpm = 82
-	audio_stream_player.stream = stream
+	_queue_track()
+
+func _queue_track() -> void:
+	rhythm_notifier.bpm = tracks[index].bpm
+	audio_stream_player.stream = tracks[index].trackStream
 	audio_stream_player.play()
 
 func _on_audio_stream_player_finished() -> void:
-	pass # Replace with function body.
-
+	index += 1
+	if index >= tracks.size():
+		index = 0
+	
+	_queue_track()
 
 func _on_rhythm_notifier_beat(current_beat: int) -> void:
 	on_beat.emit()
---
+++ scripts/track.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Track
+
+@export var trackStream: Resource
+@export var bpm: int
